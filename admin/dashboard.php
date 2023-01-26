267d50a44b761a22862caf4320b1d707ec2fe66a
Grace <gracemuig18@gmail.com>
2023-01-26 13:06:46 +0300

    init

---
+++ admin/dashboard.php
@@ -0,0 +1,63 @@
+<?php
+
+include '../components/connect.php';
+
+session_start(); 
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+    header('location:admin_login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+
+    
+    <!--swipper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
+    <!--font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <!--font google-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Andika:ital@0;1&family=Lato:ital@0;1&family=Roboto:ital,wght@0,400;1,500&display=swap" rel="stylesheet">
+
+    <!-- custom css file -->
+    <link rel="stylesheet" href="../css/admin_style.css">
+    
+
+
+</head>
+<body>
+
+<?php 
+
+include ('../components/admin_header.php');
+
+?>
+    
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file -->
+<script src="../js/admin_script.js"></script>
+    
+</body>
+</html>
